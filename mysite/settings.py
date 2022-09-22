40e17dccb8956a6275a356181e0bb0fe397b2955
cosmos1030 <dyk6208@gmail.com>
2022-09-22 01:13:07 +0000

    d

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['dyk6208.pythonanywhere.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'hello',
+    'practice',
     'mysite',
     'blog',
     'django.contrib.admin',
@@ -126,5 +128,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_ORIGINS = ['https://8000-cosmos1030-templatepyth-rvga5jaahjw.ws-us54.gitpod.io', 'https://*', 'https://dyk6208.pythonanywhere.com','https://8000-cosmos1030-djangopracti-7bcg22vcl7m.ws-us54.gitpod.io', 'https://8000-cosmos1030-djangopracti-6bvugxsacce.ws-us54.gitpod.io']
+ALLOWED_ORIGINS = ['https://8000-cosmos1030-templatepyth-rvga5jaahjw.ws-us54.gitpod.io', 'https://*', 'https://dyk6208.pythonanywhere.com','https://8000-cosmos1030-djangopracti-7bcg22vcl7m.ws-us54.gitpod.io', 'https://8000-cosmos1030-djangopracti-6bvugxsacce.ws-us54.gitpod.io', 'https://8000-cosmos1030-djangogirlsd-qq60k11rkfl.ws-us67.gitpod.io',]
 CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('practice/', include('practice.urls')),
+    path('hello/', include('hello.urls')),
 ]
